289cd8a2c46e2c2f3f22a3213cb4ad7c4c362eb1
randre03 <roger.randall@gmail.com>
2014-11-26 18:38:12 -0600

    complete through ch 11

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def hidden_div_if(condition, attributes = {}  , &block)
+    if condition
+      attributes["stytle"] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
 end
